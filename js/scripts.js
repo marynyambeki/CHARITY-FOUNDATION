d91e837a53ac7b8da24c7c7a2379932e975ee1ea
mary <marynyambeki1@gmail.com>
2019-05-16 15:01:06 +0300

    made changes to the questions page

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
-    $(".dropbtn1").click(function(){
+    $('#tab0').click(function (event) {
+        event.preventDefault();
         $(".para1").toggle();
     });
-});
-$(document).ready(function(){
-    $(".dropbtn2").click(function(){
+    $('#tab1').click(function (event) {
+        event.preventDefault();
         $(".para2").toggle();
+        $(".para1").hide();
     });
-});
-$(document).ready(function(){
-    $(".dropbtn3").click(function(){
+    $('#tab2').click(function (event) {
+        event.preventDefault();
         $(".para3").toggle();
+        $(".para2").hide();
     });
-});
-$(document).ready(function(){
-    $(".dropbtn4").click(function(){
+    $('#tab3').click(function (event) {
+        event.preventDefault();
         $(".para4").toggle();
+        $(".para3").hide();
+
     });
+    $('#tab4').click(function (event) {
+        event.preventDefault();
+        $(".para5").toggle();
+        $(".para4").hide();
+    });
+    $('#tab5').click(function (event) {
+        event.preventDefault();
+        $(".para6").toggle();
+        $(".para5").hide();
+    });
+    
 });
