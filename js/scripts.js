8300da2517274b20d0b7269beb3c6b6605a4acd6
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-05-17 10:27:33 +0300

    Upload asset files for about

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,18 @@
-// Business logic:
+// owl carousel script
 
-var maembe = function(name) {
-  return name;
- };
- 
+$(document).ready(function() {
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
+	  items: 4,
+	  loop: true,
+	  margin: 10,
+	  autoplay: true,
+	  autoplayTimeout: 1000,
+	  autoplayHoverPause: true
+	});                       
+	})
+// end of carousel script
 
 
-// Front-end logic
-$(document).ready(function(){
-	$("#work1").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#ris").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#work3").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#rispa").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#work5").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#work6").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#work7").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	$("#work8").hover(function() {
-		$(this).animate({opacity: 0.5}, 500);
-	}, function() {
-		$(this).animate({opacity: 1.0}, 500);
-	});
-	// mouseOver display hidden div
-	$('.description-wwd').mouseover(function() {
-		$(this).children(".icon-wwd").hide();
-		$(this).children(".textwwd").show();
-	}).mouseout(function() {
-		$(this).children(".icon-wwd").show();
-		$(this).children(".textwwd").hide();
-	});
-// form thank you user message
-	$("form#fomu").submit(function(event) {
-		event.preventDefault();
-		var jina = $("#name").val();
-		var maili = $("#email").val();
-		var result = maembe("Thank you " + jina + " of email address: " +  maili + " for your message. Your feedback is appreciated");
-		$("#matokeo").text(result);
-		document.getElementById("fomu").reset();
-	}); 
-});
 
 
